1d4a054093eca5376a2050355e24fe2a2361a8fd
Roman Chuchev <romanok20@gmail.com>
2022-12-16 12:25:23 +0300

    feat: update page projects

--- js/hover.js
+++ js/hover.js
@@ -14,33 +14,3 @@ document.querySelectorAll(".btn__inner").forEach((btn) => {
   });
 });
 
-// Card
-
-function map(val, minA, maxA, minB, maxB) {
-  return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
-}
-
-function Card3D(card, ev) {
-  let img = card.querySelector(".portfolio-item-inner");
-  let imgRect = card.getBoundingClientRect();
-  let mouseX = ev.offsetX;
-  let mouseY = ev.offsetY;
-  let rotateY = map(mouseX, 0, imgRect.width, -20, 20);
-  let rotateX = map(mouseY, 0, imgRect.height, 20, -20);
-
-  img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-}
-
-let cards3d = document.querySelectorAll(".card3d");
-
-cards3d.forEach((card) => {
-  card.addEventListener("mousemove", (ev) => {
-    Card3D(card, ev);
-  });
-
-  card.addEventListener("mouseleave", (ev) => {
-    let img = card.querySelector(".portfolio-item-inner");
-    img.style.transform = "rotateX(0deg) rotateY(0deg)";
-    img.style.filter = "brightness(1)";
-  });
-});
